40578e3e6aaa2cb78b99c038f5b0f86db4748562
Kim Lima <mikamil747@gmail.com>
2023-03-17 13:40:52 -0300

    Update extractorPDF.py

--- extractorPDF.py
+++ extractorPDF.py
@@ -2,11 +2,10 @@ from bs4 import BeautifulSoup
 
 import os, re, requests, time
 
-# Exemplo de url que deve ser inserida na lista
-urls = ["https://www.google.com/search?q=%22naruto%22+filetype%3A+pdf&oq=%22naruto%22+filetype%3A+pdf&aqs=chrome..69i57.13079j0j7&sourceid=chrome&ie=UTF-8"]
+urls = ["https://www.google.com/search?q=%22naruto%22+filetype%3A+pdf&oq=%22&aqs=chrome.2.69i59l3j69i57j69i59.1864j0j7&sourceid=chrome&ie=UTF-8"]
 
 # Local que contém respectivamente o path da pasta que está sendo utilizada para testes do extractor e a pasta que está sendo alimentada com os pdf catalogados.
-pathPastaLocal = input("insira o caminho da pasta que armazenará os arquivos: ")
+pathPastaLocal = input("Insira o caminho da pasta que armazenará os arquivos: ")
 pause = time.sleep(1)
 
 # Captura dos links páginas de busca do google.
